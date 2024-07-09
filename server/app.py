ae661622ed7d57bf6558ae296ec3487629e11923
Abdulhakimkullow <abdulhakim.kullow@student.moringaschool.com>
2024-07-09 10:01:39 +0300

    complete

--- server/app.py
+++ server/app.py
@@ -27,8 +27,25 @@ def index_articles():
 
 @app.route('/articles/<int:id>')
 def show_article(id):
+    session['page_views'] = session.get('page_views', 0)
 
-    pass
+    session['page_views'] += 1
+
+    if session['page_views'] <= 3:
+        article = Article.query.get(id)
+        if article:
+            return jsonify({
+                'title': article.title,
+                'content': article.content,
+                'author': article.author,
+                'preview': article.preview,
+                'minutes_to_read':article.minutes_to_read,
+                'date': article.date
+            })
+        else:
+            return jsonify({'message': 'Article not found'}), 404
+    else:
+        return jsonify({'message': 'Maximum pageview limit reached'}), 401
 
 if __name__ == '__main__':
     app.run(port=5555)
